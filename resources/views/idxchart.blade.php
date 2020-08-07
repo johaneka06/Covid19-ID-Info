9e9f4a326fc80afc28e564cbb6cbbeb4ffd0d2fb
Johan Santosa <johanekasantosa@yahoo.com>
2020-08-07 18:20:46 +0700

    Add chart

--- resources/views/idxchart.blade.php
+++ resources/views/idxchart.blade.php
@@ -4,6 +4,7 @@
   </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script type="text/javascript">
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -43,4 +44,3 @@
     options: {}
   });
 </script>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
